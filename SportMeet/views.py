f3a4894294d99965844641feae1ceaaa19510cc9
Eilon Cohen <eiloncohen285@gmail.com>
2021-10-14 22:18:33 +0300

    create initial views

--- SportMeet/views.py
+++ SportMeet/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from SportMeet.models import Profile
+from SportMeet.serializers import ProfileSerializer
 
-# Create your views here.
+class ListProfilesView(APIView):
+
+    def get(self, request, *args, **kwargs):
+        profiles = Profile.objects.all()
+        serializer = ProfileSerializer(profiles, many=True)
+        profiles_data = serializer.data
+        return Response(data=profiles_data, status=status.HTTP_200_OK)
