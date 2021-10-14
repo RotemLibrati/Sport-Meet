362bd09df2970dd26e8a43fd806a0a6630c2a2ea
Eilon Cohen <eiloncohen285@gmail.com>
2021-10-14 23:16:14 +0300

    refactor view to user selector module

--- SportMeet/views.py
+++ SportMeet/views.py
@@ -1,13 +1,14 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from SportMeet.models import Profile
 from SportMeet.serializers import ProfileSerializer
+from SportMeet import selectors
+
 
 class ListProfilesView(APIView):
 
     def get(self, request, *args, **kwargs):
-        profiles = Profile.objects.all()
+        profiles = selectors.ProfileSelector.all_objects()
         serializer = ProfileSerializer(profiles, many=True)
         profiles_data = serializer.data
         return Response(data=profiles_data, status=status.HTTP_200_OK)
