9314e2705343484d4d336e6c045d42e1c0a323d1
Eilon Cohen <eiloncohen285@gmail.com>
2021-10-14 23:02:10 +0300

    SPOR-4 registered Team and TeamPrivileges to admin page

--- SportMeet/admin.py
+++ SportMeet/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from SportMeet.models import Profile, Team, TeamPrivileges
 
-# Register your models here.
+
+class TeamAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'sport')
+
+class TeamPrivilegesAdmin(admin.ModelAdmin):
+    list_display = ('team', 'member', 'can_invite_new_members')
+
+admin.site.register(Team, TeamAdmin)
+admin.site.register(TeamPrivileges, TeamPrivilegesAdmin)
