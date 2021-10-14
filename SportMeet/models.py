0330a01c138c5d14abdcb143fdaf251a57b2b932
Eilon Cohen <eiloncohen285@gmail.com>
2021-10-14 22:40:59 +0300

    SPOR-3 created Team and TeamPrivileges models

--- SportMeet/models.py
+++ SportMeet/models.py
@@ -3,3 +3,13 @@ from django.db import models
 # Create your models here.
 class Profile(models.Model):
     email = models.CharField(max_length=100, null=True, blank=True, unique=True)
+
+
+class Team(models.Model):
+    name = models.CharField(max_length=50, null=True, blank=True)
+    sport = models.CharField(max_length=50, choices=[('football', 'football'), ('basketball', 'basketball'), ('tennis', 'tennis')])
+    
+class TeamPrivileges(models.Model):
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+    member = models.OneToOneField(Profile, on_delete=models.CASCADE)
+    can_invite_new_members = models.BooleanField(default=False, blank=True)
