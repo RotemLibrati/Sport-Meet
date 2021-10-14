bdd6cc4fee2940ca14528497b245c51aee2086b4
RotemLibrati <rotemlibrati1@gmail.com>
2021-10-14 23:50:18 +0300

    Update profile model

--- SportMeet/models.py
+++ SportMeet/models.py
@@ -2,7 +2,11 @@ from django.db import models
 
 # Create your models here.
 class Profile(models.Model):
+    SEX = (('male', 'male'), ('female', 'female'))
     email = models.CharField(max_length=100, null=True, blank=True, unique=True)
+    address = models.CharField(max_length=100, default='')
+    sex = models.CharField(max_length=10, choices=SEX)
+    age = models.IntegerField(default=0)
 
 
 class Team(models.Model):
