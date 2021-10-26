ba2ee61f6661f52ff1240df6eaccd6fb0e1842fe
Eilon Cohen <eiloncohen285@gmail.com>
2021-10-26 20:48:57 +0300

    add selectors to Game and Team models

--- SportMeet/selectors.py
+++ SportMeet/selectors.py
@@ -1,6 +1,6 @@
-from SportMeet.models import Profile, Team
+from SportMeet.models import Profile, Team, Game
 from django.contrib.auth.models import User
-
+from datetime import datetime
 
 
 class ProfileSelector:
@@ -17,7 +17,29 @@ class ProfileSelector:
             raise e
         return profile
 
+
 class UserSelector:
     @staticmethod
     def all_users():
         return User.objects.all()
+
+
+class GameSelector:
+
+    @staticmethod
+    def three_obj_in_the_future_by_username(username: str):
+        user = User.objects.get(username=username)
+        teams = user.profile.team.all()
+        _now = datetime.now()
+        games = Game.objects.filter(
+            team__in=teams, event_time__gte=_now).order_by('event_time')[0:4]
+        return games
+
+
+class TeamSelector:
+
+    @staticmethod
+    def three_obj_by_username(username):
+        user: User = User.objects.get(username=username)
+        teams = user.profile.team.all()[0:4]
+        return teams
