9f2e896e750099ee65f0cac762d8e91898df1570
Eilon Cohen <eiloncohen285@gmail.com>
2021-10-26 20:06:24 +0300

    registration view to create new user and profile

--- SportMeet/views.py
+++ SportMeet/views.py
@@ -3,11 +3,9 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from SportMeet.serializers import ProfileSerializer, UserSerializer
-from SportMeet import selectors
+from SportMeet import db_updater, selectors
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from django.contrib.auth import authenticate, login, logout
-
-
 
 
 class ListProfilesView(APIView):
@@ -19,8 +17,10 @@ class ListProfilesView(APIView):
         profiles_data = serializer.data
         return Response(data=profiles_data, status=status.HTTP_200_OK)
 
+
 class ListUsersView(APIView):
-    permission_classes= [AllowAny]
+    permission_classes = [AllowAny]
+
     def get(self, request, *args, **kwargs):
         users = selectors.UserSelector.all_users()
         serializer = UserSerializer(users, many=True)
@@ -29,7 +29,8 @@ class ListUsersView(APIView):
 
 
 class LoginView(APIView):
-    permission_classes = [AllowAny]
+    authentication_classes = []
+    permission_classes = []
 
     def post(self, request, *args, **kwargs):
         username = request.data.get('username', None)
@@ -43,10 +44,33 @@ class LoginView(APIView):
             return Response(status.HTTP_200_OK)
         return Response(data={'error': 'bad credentials'}, status=status.HTTP_400_BAD_REQUEST)
 
+
 class LogoutView(APIView):
     authentication_classes = []
     permission_classes = []
-    
+
     def post(self, request, *args, **kwargs):
         logout(request)
         return Response(status=status.HTTP_200_OK)
+
+
+class RegisterView(APIView):
+    authentication_classes = []
+    permission_classes = []
+
+    def post(self, request, *args, **kwargs):
+        user_serializer = UserSerializer(data=request.data)
+        profile_serializer = ProfileSerializer(data=request.data)
+        if user_serializer.is_valid():
+            user = db_updater.UserUpdater.create_new_user(
+                data=user_serializer.data)
+            if profile_serializer.is_valid():
+                db_updater.ProfileUpdater.create_new_profile_for_user(
+                    user, profile_serializer.data)
+            try:
+                profile_data = profile_serializer.data
+            except Exception as e:
+                profile_data = None
+            return Response(data={'user': user_serializer.data, 'profile': profile_data}, status=status.HTTP_201_CREATED)
+        else:
+            return Response(data={'errors': f'{user_serializer.errors}'}, status=status.HTTP_400_BAD_REQUEST)
