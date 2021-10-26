5cb1808e22df274aff2494a654481ecb11201b1b
Eilon Cohen <eiloncohen285@gmail.com>
2021-10-26 20:06:03 +0300

    add register url

--- SportMeet/urls.py
+++ SportMeet/urls.py
@@ -7,6 +7,5 @@ urlpatterns = [
     path('login', views.LoginView.as_view()),
     path('logout', views.LogoutView.as_view()),
     path('users', views.ListUsersView.as_view()),
-
-    
+    path('register', views.RegisterView.as_view()),
 ]
