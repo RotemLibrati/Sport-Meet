b24e25f4b8d24beb4b6b649f682671447ef97605
RotemLibrati <rotemlibrati1@gmail.com>
2021-11-20 17:03:14 +0200

    Format of datetime

--- Config/settings.py
+++ Config/settings.py
@@ -72,7 +72,8 @@ REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': [
         'rest_framework.renderers.JSONRenderer',
         'rest_framework.renderers.BrowsableAPIRenderer',
-    ]
+    ],
+    'DATETIME_FORMAT': "%Y-%m-%d %H:%M"
 }
 
 AUTHENTICATION_BACKENDS = [
