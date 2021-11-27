7f7ad11ee025421c29bbd70250f41d3e0937d189
RotemLibrati <rotemlibrati1@gmail.com>
2021-11-27 16:41:50 +0200

    Add attendance field and gamefield admin

--- SportMeet/admin.py
+++ SportMeet/admin.py
@@ -1,13 +1,18 @@
 from django.contrib import admin
-from SportMeet.models import Profile, Team, Game, GameField, AppMessage
+from SportMeet.models import Profile, Team, Game, GameField, AppMessage, Attendance
 
 
 class TeamAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'sport')
 
+class GameFieldAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'region','is_for_football', 'is_for_basketball', 'is_for_tennis')
+
 
 admin.site.register(Team, TeamAdmin)
 admin.site.register(Profile)
 admin.site.register(Game)
-admin.site.register(GameField)
+admin.site.register(GameField, GameFieldAdmin)
 admin.site.register(AppMessage)
+admin.site.register(Attendance)
+
